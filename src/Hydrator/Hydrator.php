1a4118502d6ca0dcfd3bef28f2651471e7392f35
Paul S <paul@oneofftech.xyz>
2017-08-18 08:02:35 +0200

    add hydrators

---
+++ src/Hydrator/Hydrator.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace oneofftech\KRegistryClient\Hydrator;
+
+
+use Psr\Http\Message\ResponseInterface;
+
+interface Hydrator
+{
+    public function hydrate(ResponseInterface $response, string $class);
+
+}
---
+++ src/Hydrator/ModelHydrator.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: developer
+ * Date: 8/18/17
+ * Time: 4:56 AM
+ */
+
+namespace oneofftech\KRegistryClient\Hydrator;
+
+
+use oneofftech\KRegistryClient\Exception\HydrationException;
+use oneofftech\KRegistryClient\Model\CreatableFromArray;
+use Psr\Http\Message\ResponseInterface;
+
+class ModelHydrator implements Hydrator
+{
+    public function hydrate(ResponseInterface $response, string $class)
+    {
+        $body = (string) $response->getBody();
+        if (strpos($response->getHeaderLine('Content-Type'), 'application/json') !== 0) {
+            throw new HydrationException('The ModelHydrator cannot hydrate with content-type of: '.$response->getHeaderLine('Content-Type'));
+        }
+
+        // deserialize into array
+        $data = json_decode($body, true);
+        if (JSON_ERROR_NONE !== json_last_error()) {
+            throw new HydrationException('Error when trying to decode response');
+        }
+
+        if (is_subclass_of($class, CreatableFromArray::class)) {
+            $object = call_user_func($class.'::createFromArray', $data);
+        } else {
+            $object = new $class($data);
+        }
+
+        return $object;
+    }
+
+}
